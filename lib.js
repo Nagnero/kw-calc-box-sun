081c423225aea0a06dbded07cd61ec1d052e2492
Nagnero <davidyeon123@naver.com>
2023-11-01 13:12:46 +0900

    feat: testing and publishing

--- lib.js
+++ lib.js
@@ -1,22 +1,15 @@
 function avg(numbers) {
     let s = numbers.reduce((prev, curr) => prev + curr, 0);
-    //let s = 0;
-    //for (let i = 0; i <numbers.length; i++) {
-     //   s += numbers[i];
-    //}
     return s / numbers.length;
 }
 
 function prime(num) {
-    // todo
     return false;
 }
 
 function fact(num) {
-    // todo
     return -1;
 }
-
 
 module.exports = {
     avg,
