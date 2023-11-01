f5098c9583a6cc6a682fba0a2d1e640c7e0f05de
Nagnero <davidyeon123@naver.com>
2023-11-01 14:03:38 +0900

    feat: add prime, factorial function

--- lib.js
+++ lib.js
@@ -4,11 +4,18 @@ function avg(numbers) {
 }
 
 function prime(num) {
-    return false;
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) { // 에라토네스의 체
+        if (num % i === 0) return false;
+    }
+    return true;
 }
 
-function fact(num) {
-    return -1;
+function fact(num) { // 재귀적으로 factorial 계산
+    if (num < 0 || num > 15) return -1; // 옳지 않은 값 입력
+    if (num === 0 || num === 1) return 1;
+
+    return num * fact(num - 1);
 }
 
 module.exports = {
--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,43 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("prime(1) should be false", () => {
+    expect(lib.prime(1)).toBe(false);
+});
+
+test("prime(2) should be true", () => {
+    expect(lib.prime(2)).toBe(true);
+});
+
+test("prime(3) should be true", () => {
+    expect(lib.prime(3)).toBe(true);
+});
+
+test("prime(100) should be false", () => {
+    expect(lib.prime(100)).toBe(false);
+});
+
+test("prime(101) should be true", () => {
+    expect(lib.prime(101)).toBe(true);
+});
+
+test("fact(0) should be 1", () => {
+    expect(lib.fact(0)).toBe(1);
+});
+
+test("fact(1) should be 1", () => {
+    expect(lib.fact(1)).toBe(1);
+});
+
+test("fact(6) should be 720", () => {
+    expect(lib.fact(6)).toBe(720);
+});
+
+test("fact(15) should be 1,307,674,368,000", () => {
+    expect(lib.fact(15)).toBe(1307674368000);
+});
+
+test("fact(-1) should be -1", () => {
+    expect(lib.fact(-1)).toBe(-1);
+});
